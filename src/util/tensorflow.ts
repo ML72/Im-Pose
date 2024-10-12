66f08e3f580e8a3cd793f952cf79db2cd9915c42
saejune04 <saejune04@gmail.com>
2024-10-12 16:01:00 -0700

    Added keypoint detection

--- src/util/tensorflow.ts
+++ src/util/tensorflow.ts
@@ -1,1 +1,19 @@
-// PUT STUFF HERE
+import * as tf from "@tensorflow/tfjs"
+import * as poseDetection from '@tensorflow-models/pose-detection';
+
+/**
+ * @param {HTMLImageElement} image the image to get keypoints for
+ * @returns {poseDetection.Pose[]} poses for the input image. Is an empty array if no pose found.
+ */
+export const detectKeypoints = async (
+  image: HTMLImageElement
+): Promise<poseDetection.Pose[]> => {
+  await tf.ready();
+  // Might have to tf.setBackend() I have no idea
+  const model = poseDetection.SupportedModels.MoveNet;
+  const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
+  const detector = await poseDetection.createDetector(model, detectorConfig);
+
+  const poses: poseDetection.Pose[] = await detector.estimatePoses(image);
+  return poses
+}
