c7691b7423146330f5adbd13de61075f86fa0bc4
Eric <eric.kh.bae@gmail.com>
2024-10-13 02:32:49 -0700

    initialized results page

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,59 +1,40 @@
-import React, { useEffect } from 'react';
-import { Box, Button, Container, Typography } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
 
-import CustomPage from '../components/CustomPage';
-import { slicer } from '../util/slicer';
-import { parseFiles } from '../util/parseFiles';
-import { useSelector } from 'react-redux';
-import { selectPicsState } from '../store/slices/pics';
-
+import CustomPage from "../components/CustomPage";
+import { slicer } from "../util/slicer";
+import { parseFiles } from "../util/parseFiles";
+import { useSelector } from "react-redux";
+import { selectPicsState } from "../store/slices/pics";
 
 const Results: React.FC = () => {
-    const res = useSelector(selectPicsState);
-    let imgs = slicer([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
-    
-    useEffect(() => {
-        
-    }, []);
+
   
+
   return (
     <CustomPage>
-        <br />
-        {(imgs.length === 0) ? 
-            <Typography variant='h4' align='center'>
-                No images found.
-            </Typography>
-            :
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                {imgs.map((row, rowKey) => {
-                    return (
-                        <Box sx={{ 
-                                display: 'flex', 
-                                width: '100vw', 
-                                justifyContent: 'space-evenly', 
-                                alignContent: 'center',
+			<Box sx={{ display: 'flex', flexDirection: 'column' }}>
+				<Box>
+					{Math.random() * 100}%
+				</Box>
+				<Box>
+					Score: {Math.random() * 100}
+				</Box>
+				<Box>
+					{Math.random() * 100 > 50? 
+						<Box>
 
-                            }}>
-                            {row.map((img, key) => {
-                                return (
-                                    <Box sx={{ 
-                                            border: '1px solid black', 
-                                            padding: 1, 
-                                            justifyContent: 'center', 
-                                            alignItems: 'center' 
-                                        }}>
-                                        <Typography>{img}</Typography>
-                                    </Box>
-                                )
-                            })}
-                        </Box>
-                    )
-                })}
-            </Box>
-        }
+						</Box>
+						:
+						<Box>
+							
+						</Box>
+					}
+				</Box>
+			</Box>
     </CustomPage>
-  )
+  );
 };
 
 export default Results;
--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,29 +1,59 @@
-import React from 'react';
-import { Button, Container, Typography } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
 
-import CustomPage from '../components/CustomPage';
-import { setNewAlert } from '../service/alert';
+import CustomPage from "../components/CustomPage";
+import { slicer } from "../util/slicer";
+import { parseFiles } from "../util/parseFiles";
+import { useSelector } from "react-redux";
+import { selectPicsState } from "../store/slices/pics";
 
 const Stats: React.FC = () => {
-  const dispatch = useDispatch();
+  const res = useSelector(selectPicsState);
+  let imgs = slicer([1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
 
-  const alertHandler = () => {
-    setNewAlert(dispatch, { msg: "Hello world!" });
-  }
-  
+  useEffect(() => {}, []);
+
   return (
     <CustomPage>
-      <Typography align="center" variant="body1" sx={{ mt: 3, mx: 2 }}>
-        This is the stats page.
-      </Typography>
-      <Container sx={{ mt: 3 }}>
-        <Button variant="contained" fullWidth onClick={alertHandler}>
-          Click me!
-        </Button>
-      </Container>
+      <br />
+      {imgs.length === 0 ? (
+        <Typography variant="h4" align="center">
+          No images found.
+        </Typography>
+      ) : (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          {imgs.map((row, rowKey) => {
+            return (
+              <Box
+                sx={{
+                  display: "flex",
+                  width: "100vw",
+                  justifyContent: "space-evenly",
+                  alignContent: "center",
+                }}
+              >
+                {row.map((img, key) => {
+                  return (
+                    <Box
+                      sx={{
+                        border: "1px solid black",
+                        padding: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Typography>{img}</Typography>
+                    </Box>
+                  );
+                })}
+              </Box>
+            );
+          })}
+        </Box>
+      )}
     </CustomPage>
-  )
+  );
 };
 
 export default Stats;
